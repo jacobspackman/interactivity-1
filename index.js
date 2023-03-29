d285b901fdd1c897398098abe09d3be42d99445b
Jacob Spackman <jacobts202@gmail.com>
2023-03-28 19:19:37 -0600

    core completed

--- index.js
+++ index.js
@@ -2,17 +2,37 @@ console.log(`Hello World`)
 
 let inputField = document.querySelector(`input`)
 let movieList = document.querySelector(`ul`)
+let message = document.querySelector(`#message`)
 
 let addMovie = (event) => {
     event.preventDefault()
     let movie = document.createElement(`li`)
     let movieTitle = document.createElement(`span`)
     movieTitle.textContent = inputField.value
+    movieTitle.addEventListener(`click`, crossOffMovie)
     movie.appendChild(movieTitle)
+    let deleteBtn = document.createElement(`button`)
+    deleteBtn.textContent = `X`
+    deleteBtn.addEventListener(`click`, deleteMovie)
+    movie.appendChild(deleteBtn)
     movieList.appendChild(movie)
     inputField.value = ``
+}
+
+let deleteMovie = (event) => {
+    event.target.parentNode.remove()
+    message.textContent = `Movie deleted!`
 }
 
 let form = document.querySelector(`form`)
 
 form.addEventListener(`submit`, addMovie)
+
+let crossOffMovie = (event) => {
+    event.target.classList.toggle('checked')
+    if(event.target.classList.contains(`checked`)){
+        message.textContent = `Movie Watched!`
+    }else{
+        message.textContent = `Movie added back!`
+    }
+}
